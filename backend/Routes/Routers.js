1868682aaba82b7ec3900e63b06ccd5c25cf75df
kapil-122414 <kapilkumar122414@gmail.com>
2026-04-01 12:32:42 +0530

    delete api
    new delete api

--- backend/Routes/Routers.js
+++ backend/Routes/Routers.js
@@ -1,6 +1,7 @@
 const modelschema = require("../models/schema");
 const express = require("express");
 const router = express.Router();
+const fs=require("fs");
 const uploads = require("../multer/imgmulter");
 
 router.post("/category", uploads.single("Img"), async (req, res) => {
@@ -9,8 +10,8 @@ router.post("/category", uploads.single("Img"), async (req, res) => {
 
     const newcategory = await modelschema.create({
       Img: req.file
-  ? req.file.path.replace(/\\/g, "/").replace(/\s/g, "")
-  : null,
+        ? req.file.path.replace(/\\/g, "/").replace(/\s/g, "")
+        : null,
       ...data,
 
       //    Img: req.file ? req.file.path.replace(/\\/g, "/") : null,
@@ -35,16 +36,29 @@ router.get("/category", async (req, res) => {
   }
 });
 
-router.delete("/category/:Categoryname ",async(req,res)=>{
-  try{ 
-    const data=req.findOne();
-  console.log(data);
-}
-catch(error){
-  res.status(500).json
-}
+router.delete("/category/:_id", async (req, res) => {
+  try {
+    const data = req.params._id;
+    
+    const finddata = await modelschema.findByIdAndDelete(data);
 
- 
+    if (!finddata) {
+      return res.status(404).json({ message: "not find data" });
+    }
+    const filepath=finddata(Img);
+    fs.unlink(filepath,(err)=>{
+      if(err){
+        console.log(" error delete file ",err)
+      }
+      else{
+        console.log("delete succesfully");
+      }
+    })
+    
+    res.status(200).json({ message: "delete successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "server error" });
+  }
+});
 
-})
 module.exports = router;

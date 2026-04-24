18ddcf37db0d0f977d5cdf0ce9c57b2bfc6f8ca0
kapil-122414 <kapilkumar122414@gmail.com>
2026-04-24 21:33:07 +0530

    add the product

---
+++ frontend/product.js
@@ -0,0 +1,6 @@
+async function openproduct() {
+  document.getElementById("productforms").style.display = "flex";
+}
+function closeproduct(){
+  document.getElementById("productforms").style.display = "none";
+}

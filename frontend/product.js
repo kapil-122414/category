b1719ffecdcf5079766e385d9a81396d8086b92f
kapil-122414 <kapilkumar122414@gmail.com>
2026-04-28 21:15:36 +0530

    fetch the post api in product

--- frontend/product.js
+++ frontend/product.js
@@ -1,5 +1,6 @@
 async function openproduct() {
   document.getElementById("productforms").style.display = "flex";
+  loadCategories();
 }
 
 async function closeproduct() {
@@ -19,7 +20,11 @@ proimg.addEventListener("change", function () {
 
 // add variant
 async function proreset() {
-  document.getElementById("proimg").value = "";
+  const preview = document.getElementById("propreview");
+
+  preview.src = "";
+  preview.style.display = "none";
+
   document.getElementById("proname").value = "";
   document.getElementById("proslug").value = "";
   document.getElementById("prodescrition").value = "";
@@ -41,7 +46,7 @@ function addVariant() {
     <input type="text" placeholder="Size" class="size" id='size'>
     <input type="text" placeholder="Color" class="color">
     <input type="number" placeholder="MRP" class="mrp" min="0">
-    <input type="number" placeholder="Discount %" class="discount" min="0">
+    <input type="number" placeholder="Discount %" class="discount" min="0" >
     <input type="number" placeholder="Price" class="price" readonly>
     <input type="number" placeholder="Stock" class="stock" min="0">
     <input type="text" placeholder="SKU" class="sku" readonly>
@@ -116,3 +121,113 @@ document.addEventListener("input", function (e) {
     box.querySelector(".sku").value = generateSKU(name, color, size);
   }
 });
+
+//get variant
+async function getvriant() {
+  const variantbox = document.querySelectorAll(".variant-box");
+  const variants = [];
+
+  variantbox.forEach((box) => {
+    const image = box.querySelector(".img").files[0];
+    const size = box.querySelector(".size").value;
+    const color = box.querySelector(".color").value;
+    const mrp = box.querySelector(".mrp").value;
+    const discount = box.querySelector(".discount").value;
+    const price = box.querySelector(".price").value;
+    const stock = box.querySelector(".stock").value;
+    const sku = box.querySelector(".sku").value;
+    variants.push({
+      image,
+      size,
+      color,
+      mrp,
+      discount,
+      price,
+      stock,
+      sku,
+    });
+  });
+  return variants;
+}
+// post api
+async function prosubmit() {
+  const variants = await getvriant();
+
+  const proname = document.getElementById("proname").value;
+  const proslug = document.getElementById("proslug").value;
+  const prodescrition = document.getElementById("prodescrition").value;
+  const proshort = document.getElementById("proshort").value;
+  const probrand = document.getElementById("probrand").value;
+  const promrp = document.getElementById("promrp").value;
+  const prodiscount = document.getElementById("prodiscount").value;
+  const proprice = document.getElementById("proprice").value;
+  const prostatus = document.getElementById("status").value;
+
+  const proImage = document.getElementById("proimg").files[0];
+  const categoryId = document.getElementById("category").value;
+
+  if (!proname || !proslug || !prodescrition || !proshort) {
+    return alert("please fill the form");
+  }
+  const formData = new FormData();
+
+  if (proImage) {
+    formData.append("Img", proImage);
+  }
+  if (!categoryId) {
+    return alert("Please select category");
+  }
+  variants.forEach((v) => {
+    if (v.image) {
+      formData.append("variantImages", v.image);
+    }
+  });
+
+  formData.append("variant", JSON.stringify(variants));
+
+  // 👉 backend ke hisaab se keys
+  formData.append("Productname", proname);
+  formData.append("slug", proslug);
+  formData.append("description", prodescrition);
+  formData.append("shortDescription", proshort);
+  formData.append("brand", probrand);
+  formData.append("mrp", promrp);
+  formData.append("discount", prodiscount);
+  formData.append("price", proprice);
+  formData.append("status", prostatus);
+  formData.append("categoryId", categoryId);
+
+  try {
+    const res = await fetch("http://localhost:5000/api/product", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      alert("successfully post data ");
+      procCancel();
+      proreset();
+    } else {
+      alert(data.message || "Error ");
+    }
+  } catch (error) {
+    console.error("error", error);
+  }
+}
+
+async function loadCategories() {
+  const res = await fetch("http://localhost:5000/api/category/all");
+  const data = await res.json();
+
+  const select = document.getElementById("category");
+
+  data.forEach((cat) => {
+    const option = document.createElement("option");
+    option.value = cat._id;
+    option.textContent = cat.Categoryname;
+
+    select.appendChild(option);
+  });
+}
